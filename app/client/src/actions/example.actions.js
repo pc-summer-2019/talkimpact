d54fb86bbab9a9d7ced0ce5ca2b0ffb8d7c8b2bb
Michael Sweeney <jfmike@gmail.com>
2019-08-14 10:54:08 -0700

    closes #1

--- app/client/src/actions/example.actions.js
+++ app/client/src/actions/example.actions.js
@@ -1,5 +1,17 @@
 import store from '../store'
 import axios from 'axios'
+import io from 'socket.io-client'
+
+// MUST CHANGE localhost to IP ADDRESS
+const socket = io.connect('http://localhost:8000')
+
+socket.on('new person', name => {
+  console.log(name)
+})
+
+export function submitName(name) {
+  socket.emit('name', name)
+}
 
 export function greet() {
   axios.get('/api/greeting').then(resp => {
